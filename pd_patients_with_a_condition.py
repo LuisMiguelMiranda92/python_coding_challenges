dc2a97fdbe47cdfa2b2e004157ff6fa4f35553c0
Miguel Miranda <lmbasmiranda@gmail.com>
2025-10-28 11:14:30 -0700

    added new challenge

--- pd_patients_with_a_condition.py
+++ pd_patients_with_a_condition.py
@@ -18,5 +18,5 @@ Return the result table in any order.
 import pandas as pd
 
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    mask = patients['conditions'].str.contains('DIAB1')
-    return patients[mask]
+ mask = patients['conditions'].str.contains(r'(^|\s)DIAB1', regex=True)
+ return patients[mask]
