bcedc2503a65c2d5f434bdddb1638b50e5409acc
Miguel Miranda <lmbasmiranda@gmail.com>
2026-01-08 10:54:50 -0800

    added new project

--- rearrange_products_table.py
+++ rearrange_products_table.py
@@ -19,8 +19,6 @@ Return the result table in any order.
 import pandas as pd
 
 def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
-    products = products.melt(id_vars=['product_id'],
-                                value_vars=['store1', 'store2', 'store3'],
-                                var_name = 'store',
-                                value_name =  'price')
-    return products.dropna(subset='price')
+    return products.melt(id_vars=['product_id'], 
+                        value_vars=['store1', 'store2', 'store3'], 
+                        var_name='store', value_name='price').dropna(subset=['price'])
