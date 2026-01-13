f68e4c0e37fd7401aed57dce107af4f04ec81b18
Miguel Miranda <lmbasmiranda@gmail.com>
2026-01-13 11:54:36 -0800

    first commit

--- two_sum.py
+++ two_sum.py
@@ -1,18 +1,41 @@
-"""
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# Two Sum
+# Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 
-You can return the answer in any order.
-"""
+# Return the answer with the smaller index first.
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for n in range(len(nums)):
-            if nums[n] + nums[n+1] == target:
-                return [n, n+1]
+# Example 1:
+
+# Input: 
+# nums = [3,4,5,6], target = 7
+
+# Output: [0,1]
+# Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+# Example 2:
+
+# Input: nums = [4,5,6], target = 10
+
+# Output: [0,2]
+# Example 3:
+
+# Input: nums = [5,5], target = 10
+
+# Output: [0,1]
+# Constraints:
+
+# 2 <= nums.length <= 1000
+# -10,000,000 <= nums[i] <= 10,000,000
+# -10,000,000 <= target <= 10,000,000
+# Only one valid answer exists.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
