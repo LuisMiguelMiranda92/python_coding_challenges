b1dbde0365c4996df87e309cb1263eefddc3477b
Miguel Miranda <lmbasmiranda@gmail.com>
2026-01-19 14:06:41 -0800

    Uploading challenge

---
+++ contains_duplicate.py
@@ -0,0 +1,21 @@
+# Contains Duplicate
+# Solved 
+# Easy
+# Company Tags
+# Hints
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Example 1:
+
+# Input: nums = [1, 2, 3, 3]
+
+# Output: true
+
+# Example 2:
+
+# Input: nums = [1, 2, 3, 4]
+
+# Output: false
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) > len(set(nums))
---
+++ longest_consecutive_sequence.py
@@ -0,0 +1,38 @@
+# Longest Consecutive Sequence
+# Medium
+# Company Tags
+# Hints
+# Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+# A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
+
+# You must write an algorithm that runs in O(n) time.
+
+# Example 1:
+
+# Input: nums = [2,20,4,10,3,4,5]
+
+# Output: 4
+# Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+# Example 2:
+
+# Input: nums = [0,3,2,5,4,6,1,1]
+
+# Output: 7
+# Constraints:
+
+# 0 <= nums.length <= 1000
+# -10^9 <= nums[i] <= 10^9
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest_seq = 0
+
+        for n in numSet:
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest_seq = max(length, longest_seq)
+        return longest_seq
